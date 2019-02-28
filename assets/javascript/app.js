46469033bf9a659ccd4c5f1cddd0f9cce20e71d1
bander <45398360+BenMakesCodeWork@users.noreply.github.com>
2019-02-27 18:37:57 -0600

    reports correct or incorrect on all 3 answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,15 @@ var questions = ["What's Ben's favorie band","Favorite cookie?","Sesame stree ch
 var options = [["Radiohead","Led Zepplin", "Alice in Chains"],["Chocolate Chip","Raisin","Oatmeal"],["Big Bird","Cookie Monster","Dracula"]];
 var answers = [0,0,1];
 var guesses = [];
-var myVar = setInterval(myTimer, 1000);
+var myVar = 0;
 
-newQuestion()
+$("#start").on("click", function() {
+    newQuestion();
+    myVar = setInterval(myTimer, 1000);
+    $("#container").removeClass(hide);
+});
+
+
 
 function myTimer() {
     counter++;
@@ -24,16 +30,20 @@ function myTimer() {
 } 
 
 function newQuestion() {
+    
+    if(index === 3) {
+        clearInterval(myVar);
+        gameOver();
+    }
+    
     $("#question").text(questions[index]);
     $("#option1").text(options[index][0]);
     $("#option2").text(options[index][1]);
     $("#option3").text(options[index][2]);
     index++;
 
-    if(index === 4) {
-        clearInterval();
-        gameOver();
-    }
+    //console.log("new Questions was called and executed, index = " + index);
+    
 
 }
 
@@ -47,8 +57,23 @@ $(".option").on("click", function(){
 function gameOver() {
     alert("Game Over!");
     if(guesses[0] === answers[0]) {
-        $("#result1").text("Correct");
+        $("#result1").text("Answer 1: Correct");
     } else {
-        $("#result1").text("Wrong");
+        $("#result1").text("Answer 1: Incorrect (correct answer is " + options[0][0] + ")");
     }
+
+    alert("past execution of first if");
+
+    if(guesses[1] === answers[1]) {
+        $("#result2").text("Answer 1: Correct");
+    } else {
+        $("#result2").text("Answer 1: Incorrect (correct answer is " + options[1][0] + ")");
+    }
+
+    if(guesses[2] === answers[2]) {
+        $("#result3").text("Answer 1: Correct");
+    } else {
+        $("#result3").text("Answer 1: Incorrect (correct answer is " + options[2][1] + ")");
+    }
+
 }
